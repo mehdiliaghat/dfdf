8a87cc97893170c59c7a6098f24ac48ebc506d0d
mehdiliaghat <mohammadmehdiliaghatzadeh@gmail.com>
2018-03-21 15:27:21 +0330

    change login title
    Signed-off-by: mehdiliaghat <mohammadmehdiliaghatzadeh@gmail.com>

--- resources/views/auth/app.blade.php
+++ resources/views/auth/app.blade.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login nejah</title>
+    <title>login</title>
     <!-- Core CSS - Include with every page -->
     <link href="plugins/bootstrap/bootstrap.css" rel="stylesheet" />
     <link href="font-awesome/css/font-awesome.css" rel="stylesheet" />
