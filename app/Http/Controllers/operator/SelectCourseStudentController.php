b20e34c6ac2c1d9f27e3f4dc051543f35501231c
mehdiliaghat <mohammadmehdiliaghatzadeh@gmail.com>
2018-06-05 15:12:37 +0430

    update new
    Signed-off-by: mehdiliaghat <mohammadmehdiliaghatzadeh@gmail.com>

--- app/Http/Controllers/operator/SelectCourseStudentController.php
+++ app/Http/Controllers/operator/SelectCourseStudentController.php
@@ -32,13 +32,7 @@ class SelectCourseStudentController extends Controller
     {
 
 
-       /* if ($assignedcourses) {
-        foreach ($assignedcourses as $assignedcourse) {
-            $unit_count = $assignedcourse->sum('unit');
-        }
-    }else
-            $unit_count = 0 ;
-       */
+
         $student = Student::find($id);
         $assignedcourses = $student->assignedcourses;
         $unit_count = $student->assignedcourses->sum('unit');
@@ -47,20 +41,21 @@ class SelectCourseStudentController extends Controller
 
     public function store(Request $request)
     {
+        // get request and check that this course is existing or not
         $data = $request->student_id;
         $student = Student::find($data);
         $assignedcourse = Assignedcourse::where('course_id', $request->course_id)
             ->where('course_gp', $request->course_gp)
             ->get();
-
-
+        // if count of $assignedcourse is 0 this course by this course_id and course_gp not existing and return error
         if (count($assignedcourse) == 000) {
             $assignedcourses = $student->assignedcourses;
-            $unit_count = $student->assignedcourses->sum('unit');
+            $unit_count = $student->assignedcourses->sum('unit');// compute count of unit that selected before this
             swal()->error('', ' اطلاعاتی درس صحیح نیست ');
             return view('operator.testselectCourseIndex', compact('assignedcourses'))
                 ->with(['unit_count' => $unit_count,'student_id' => $data ]);
         } else {
+            //if count of $assignedcourse not 0 check that this course  selected before or not
             $assignedcourse_id = DB::table('assignedcourses')
                 ->where('course_id', $request->course_id)
                 ->where('course_gp', $request->course_gp)
@@ -69,15 +64,210 @@ class SelectCourseStudentController extends Controller
                     ->whereAssignedcourseId($assignedcourse_id)
                     ->whereStudentId($data)
                     ->get();
-
+            //if count of $exists is 0 means this course not selected before
             if(count($exists)==0) {
-                $student->assignedcourses()->attach($assignedcourse_id);
-                $assignedcourses = $student->assignedcourses;
-                $unit_count = $student->assignedcourses->sum('unit');
+                $student_term = Student::where('id',$data)->first()->term;
+                //this check previous term ave
+                if($student_term%10==1){
+                    //if present term in one autumnal term
+                    $Previous_term=$student_term-10;//for find previous term
+                    $user_id=Student::where('id',$data)->first()->user_id;
+                    $ave_term=Student::where('user_id',$user_id)
+                        ->where('term',$Previous_term)->first()->ave_term;
+                    //if ave_term for previous term is <12
+                    if($ave_term<12){
+                        $max=14;
+                        $assignedcourse_unit = DB::table('assignedcourses')
+                            ->where('course_id', $request->course_id)
+                            ->where('course_gp', $request->course_gp)
+                            ->first()->unit;
+                        $assignedcourses = $student->assignedcourses;
+                        //sum previous selected unit course by present unit  course for compute
+                        $p_unit_count = $student->assignedcourses->sum('unit')+$assignedcourse_unit ;
+                        $unit_count=$student->assignedcourses->sum('unit');
+                        //if $unit_count > of 14
+                        if($p_unit_count>$max){
+                            swal()->error('', ' شما مجاز به انتخاب بیش از 14 واحد نیستید  ');
+                            return view('operator.testselectCourseIndex', compact('assignedcourses') ,
+                                ['unit_count' => $unit_count,'student_id' => $data ]);
+                        }else{
+                            $student->assignedcourses()->attach($assignedcourse_id);
+                            $assignedcourses = $student->assignedcourses;
+                            $unit_count = $student->assignedcourses->sum('unit') ;
+                            $student->unit_term=$unit_count;
+                            $student->save();
+                            swal()->error('', '  درس با موفقیت ثبت شد');
+                            return view('operator.testselectCourseIndex', compact('assignedcourses') ,
+                                ['unit_count' => $unit_count,'student_id' => $data ]);
+
+                        }
+
+                    }elseif ($ave_term>=17){
+                        $max=24;
+                        $assignedcourse_unit = DB::table('assignedcourses')
+                            ->where('course_id', $request->course_id)
+                            ->where('course_gp', $request->course_gp)
+                            ->first()->unit;
+                        $assignedcourses = $student->assignedcourses;
+                        $p_unit_count = $student->assignedcourses->sum('unit')+$assignedcourse_unit ;
+                        $unit_count=$student->assignedcourses->sum('unit');
+                        if($p_unit_count>$max){
+                            swal()->error('', ' شما مجاز به انتخاب بیش از 24 واحد نیستید  ');
+                            return view('operator.testselectCourseIndex', compact('assignedcourses') ,
+                                ['unit_count' => $unit_count,'student_id' => $data ]);
+                        }else{
+                            $student->assignedcourses()->attach($assignedcourse_id);
+                            $assignedcourses = $student->assignedcourses;
+                            $unit_count = $student->assignedcourses->sum('unit') ;
+                            $student->unit_term=$unit_count;
+                            $student->save();
+                            swal()->error('', '  درس با موفقیت ثبت شد');
+                            return view('operator.testselectCourseIndex', compact('assignedcourses') ,
+                                ['unit_count' => $unit_count,'student_id' => $data ]);
+
+                        }
+
+                    }else{
+                        $max=20;
+                        $assignedcourse_unit = DB::table('assignedcourses')
+                            ->where('course_id', $request->course_id)
+                            ->where('course_gp', $request->course_gp)
+                            ->first()->unit;
+                        $assignedcourses = $student->assignedcourses;
+                        $p_unit_count = $student->assignedcourses->sum('unit')+$assignedcourse_unit ;
+                        $unit_count=$student->assignedcourses->sum('unit');
+                        if($p_unit_count>$max){
+                            swal()->error('', ' شما مجاز به انتخاب بیش از 20 واحد نیستید  ');
+                            return view('operator.testselectCourseIndex', compact('assignedcourses') ,
+                                ['unit_count' => $unit_count,'student_id' => $data ]);
+                        }else{
+                            $student->assignedcourses()->attach($assignedcourse_id);
+                            $assignedcourses = $student->assignedcourses;
+                            $unit_count = $student->assignedcourses->sum('unit') ;
+                            $student->unit_term=$unit_count;
+                            $student->save();
+                            swal()->error('', '  درس با موفقیت ثبت شد');
+                            return view('operator.testselectCourseIndex', compact('assignedcourses') ,
+                                ['unit_count' => $unit_count,'student_id' => $data ]);
+
+                        }
+
+                    }
 
 
-                    return view('operator.testselectCourseIndex', compact('assignedcourses') ,
-                        ['unit_count' => $unit_count,'student_id' => $data ]);
+                }elseif($student_term%10==2){
+                    $Previous_term=$student_term-1;
+                    $user_id=Student::where('id',$data)->first()->user_id;
+                    $ave_term=Student::where('user_id',$user_id)
+                        ->where('term',$Previous_term)->first()->ave_term;
+                    if($ave_term<12){
+                        $max=14;
+                        $assignedcourse_unit = DB::table('assignedcourses')
+                            ->where('course_id', $request->course_id)
+                            ->where('course_gp', $request->course_gp)
+                            ->first()->unit;
+                        $assignedcourses = $student->assignedcourses;
+                        $p_unit_count = $student->assignedcourses->sum('unit')+$assignedcourse_unit ;
+                        $unit_count=$student->assignedcourses->sum('unit');
+                        if($p_unit_count>$max){
+                            swal()->error('', ' شما مجاز به انتخاب بیش از 14 واحد نیستید  ');
+                            return view('operator.testselectCourseIndex', compact('assignedcourses') ,
+                                ['unit_count' => $unit_count,'student_id' => $data ]);
+                        }else{
+                            $student->assignedcourses()->attach($assignedcourse_id);
+                            $assignedcourses = $student->assignedcourses;
+                            $unit_count = $student->assignedcourses->sum('unit') ;
+                             $student->unit_term=$unit_count;
+                             $student->save();
+                            swal()->error('', '  درس با موفقیت ثبت شد');
+                            return view('operator.testselectCourseIndex', compact('assignedcourses') ,
+                                ['unit_count' => $unit_count,'student_id' => $data ]);
+
+                        }
+
+                    }elseif ($ave_term>=17){
+                        $max=24;
+                        $assignedcourse_unit = DB::table('assignedcourses')
+                            ->where('course_id', $request->course_id)
+                            ->where('course_gp', $request->course_gp)
+                            ->first()->unit;
+                        $assignedcourses = $student->assignedcourses;
+                        $p_unit_count = $student->assignedcourses->sum('unit')+$assignedcourse_unit ;
+                        $unit_count=$student->assignedcourses->sum('unit');
+                        if($p_unit_count>$max){
+                            swal()->error('', ' شما مجاز به انتخاب بیش از 24 واحد نیستید  ');
+                            return view('operator.testselectCourseIndex', compact('assignedcourses') ,
+                                ['unit_count' => $unit_count,'student_id' => $data ]);
+                        }else{
+                            $student->assignedcourses()->attach($assignedcourse_id);
+                            $assignedcourses = $student->assignedcourses;
+                            $unit_count = $student->assignedcourses->sum('unit') ;
+                            $student->unit_term=$unit_count;
+                            $student->save();
+                            swal()->error('', '  درس با موفقیت ثبت شد');
+                            return view('operator.testselectCourseIndex', compact('assignedcourses') ,
+                                ['unit_count' => $unit_count,'student_id' => $data ]);
+
+                        }
+
+                    }else{
+                        $max=20;
+                        $assignedcourse_unit = DB::table('assignedcourses')
+                            ->where('course_id', $request->course_id)
+                            ->where('course_gp', $request->course_gp)
+                            ->first()->unit;
+                        $assignedcourses = $student->assignedcourses;
+                        $p_unit_count = $student->assignedcourses->sum('unit')+$assignedcourse_unit ;
+                        $unit_count=$student->assignedcourses->sum('unit');
+                        if($p_unit_count>$max){
+                            swal()->error('', ' شما مجاز به انتخاب بیش از 20 واحد نیستید  ');
+                            return view('operator.testselectCourseIndex', compact('assignedcourses') ,
+                                ['unit_count' => $unit_count,'student_id' => $data ]);
+                        }else{
+                            $student->assignedcourses()->attach($assignedcourse_id);
+                            $assignedcourses = $student->assignedcourses;
+                            $unit_count = $student->assignedcourses->sum('unit') ;
+                            $student->unit_term=$unit_count;
+                            $student->save();
+                            swal()->error('', '  درس با موفقیت ثبت شد');
+                            return view('operator.testselectCourseIndex', compact('assignedcourses') ,
+                                ['unit_count' => $unit_count,'student_id' => $data ]);
+
+                        }
+
+                    }
+
+
+                }elseif($student_term%10==3){
+                    $max=6;
+                    $assignedcourse_unit = DB::table('assignedcourses')
+                        ->where('course_id', $request->course_id)
+                        ->where('course_gp', $request->course_gp)
+                        ->first()->unit;
+                    $assignedcourses = $student->assignedcourses;
+                    $p_unit_count = $student->assignedcourses->sum('unit')+$assignedcourse_unit ;
+                    $unit_count=$student->assignedcourses->sum('unit');
+                    if($p_unit_count>$max){
+                        swal()->error('', ' شما مجاز به انتخاب بیش از 6 واحد نیستید  ');
+                        return view('operator.testselectCourseIndex', compact('assignedcourses') ,
+                            ['unit_count' => $unit_count,'student_id' => $data ]);
+                    }else{
+                        $student->assignedcourses()->attach($assignedcourse_id);
+                        $assignedcourses = $student->assignedcourses;
+                        $unit_count = $student->assignedcourses->sum('unit') ;
+                        $student->unit_term=$unit_count;
+                        $student->save();
+                        swal()->error('', '  درس با موفقیت ثبت شد');
+                        return view('operator.testselectCourseIndex', compact('assignedcourses') ,
+                            ['unit_count' => $unit_count,'student_id' => $data ]);
+
+                    }
+
+                }else{
+                    swal()->error('', ' خطا در ثبت ترم  ');
+                    return view('operator.testshowTermForSelectCourse',$data);
+                }
+
 
             }else{
                 $assignedcourses = $student->assignedcourses;
@@ -88,17 +278,6 @@ class SelectCourseStudentController extends Controller
 
             }
         }
-
-
-
-
-        /* if ($assignedcourses) {
-             foreach ($assignedcourses as $assignedcourse) {
-                 $unit_count = $assignedcourse->sum('unit');
-             }
-         }*/
-
-
 
     }
     public function delete($id ,$student_id)
